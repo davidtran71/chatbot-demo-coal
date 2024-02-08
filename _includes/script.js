84d9522fd0c1bbe21bc1b2d5410c6051f502d2ff
davidtran71 <davidtran07@gmail.com>
2024-02-07 23:48:07 -0500

    Update
    Update

--- _includes/script.js
+++ _includes/script.js
@@ -1,1 +1,11 @@
-console.log("Micro-site");
+
+
+function showStackUp() {
+  // Hide the input fields and button
+  document.getElementById("input1").style.display = "none";
+  document.getElementById("input2").style.display = "none";
+  document.querySelector("button").style.display = "none";
+
+  // Show the stack up information
+  document.getElementById("stackUpInfo").classList.remove("hidden");
+}
